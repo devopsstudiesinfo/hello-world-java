e55ceb7db6c11b427579afa63ac1caeedd3fb3dd
madhu <jmstechhome6@gmail.com>
2020-04-03 14:49:20 +0000

    remove helloworld line

--- HelloWorld.java
+++ HelloWorld.java
@@ -34,13 +34,5 @@
  */
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, World");
     System.out.println("Hello, corona");
     System.out.println("go, corona");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-  }
-}
